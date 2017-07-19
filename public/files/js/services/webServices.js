08fe24bcb33d6c2ce648be951324dce930f1a0ca
Ala Shehadeh <alaa.sh.1984@gmail.com>
2017-07-19 21:18:09 +0300

    no message

--- public/files/js/services/webServices.js
+++ public/files/js/services/webServices.js
@@ -1,5 +1,5 @@
 tradeTracker.service('webApi', function ($http) {
     this.searchResult = function (data) {
-        return $http.post('/api/v1/formData',data);
+        return $http.post('https://tranquil-ravine-89010.herokuapp.com/public/api/v1/formData',data);
     }
 });
